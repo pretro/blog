334eae440bcda1f7db16f8de0401deae45aadc82
jort2 <>
2023-03-14 19:23:03 +0100

    creating subfolders in the component folder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import Topbar from "./components/topbar/Topbar";
+import Homepage from "./pages/homepage/Homepage";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <Router>
     <Topbar />
       <Routes>
-        
+      <Route exact path="/">
+        <Homepage />
+      </Route>
+      <Route path="/posts">
+        <Homepage />
+      </Route>
       </Routes>
     </Router>
   );
