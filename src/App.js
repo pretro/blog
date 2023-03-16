313b01f154bed8933a3c51686770db02759e410e
jort2 <>
2023-03-16 21:35:59 +0100

    Created login and register folder and files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import TopBar from './components/topbar/TopBar'
 import Homepage from "./pages/homepage/Homepage";
+import Single from "./pages/single/Single";
+import Register from "./pages/register/Register";
+import Write from "./pages/write/Write";
+import Login from "./pages/login/Login";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 
 const App = () => {
+  const currentUser = true;
   return (
     <Router>
         <TopBar/>
         <Routes>
             <Route path="/" element={<Homepage />}/>
             <Route path="/posts" element={<Homepage />}/>
+            <Route path="/post/:id" element={<Single />}/>
+            <Route path="/write" element={<Write/>}/>
+            <Route path="/register" element= {currentUser ? <Homepage /> : <Register />}/>
+            <Route path="/login" element= {currentUser ? <Homepage /> : <Login />}/>
         </Routes>
     </Router>
         
